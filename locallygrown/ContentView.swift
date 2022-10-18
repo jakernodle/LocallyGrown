2e91c601c1d7fb434f235ce49ae5df51ff53328b
JohnAnge Kernodle <jakernodle@gmail.com>
2022-10-18 16:04:12 -0400

    Initial Commit

---
+++ locallygrown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  locallygrown
+//
+//  Created by JA Kernodle on 10/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ locallygrown/locallygrownApp.swift
@@ -0,0 +1,17 @@
+//
+//  locallygrownApp.swift
+//  locallygrown
+//
+//  Created by JA Kernodle on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct locallygrownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
